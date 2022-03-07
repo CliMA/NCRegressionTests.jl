b17ab7dc60eca4abf48684781e7218c9a87799e9
Charles Kawczynski <kawczynski.charles@gmail.com>
2022-03-07 11:35:52 -0800

    Add src

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # NCRegressionTests.jl
+
 A package for regression tests over NCDatasets.
+
+|||
+|---------------------:|:----------------------------------------------|
+| **GHA CI**           | [![gha ci][gha-ci-img]][gha-ci-url]           |
+| **Code Coverage**    | [![codecov][codecov-img]][codecov-url]        |
+| **Bors enabled**     | [![bors][bors-img]][bors-url]                 |
+
+[gha-ci-img]: https://github.com/CliMA/NCRegressionTests.jl/actions/workflows/ci.yml/badge.svg
+[gha-ci-url]: https://github.com/CliMA/NCRegressionTests.jl/actions/workflows/ci.yml
+
+[codecov-img]: https://codecov.io/gh/CliMA/NCRegressionTests.jl/branch/main/graph/badge.svg
+[codecov-url]: https://codecov.io/gh/CliMA/NCRegressionTests.jl
+
+[bors-img]: https://bors.tech/images/badge_small.svg
+[bors-url]: https://app.bors.tech/repositories/42467
+
+## Example
+
+See our [tests](https://github.com/CliMA/NCRegressionTests.jl/blob/main/test/runtests.jl) for example usage.
+
---
+++ test/runtests.jl
@@ -0,0 +1,93 @@
+using Test
+using Random
+using NCDatasets
+using OrderedCollections
+using NCRegressionTests
+Random.seed!(123)
+
+@testset "NCRegressionTests - perfect match" begin
+
+    sz = (123, 145)
+    data = randn(MersenneTwister(152), sz)
+
+    filenameA = tempname()
+    filenameB = tempname()
+
+    NCDataset(filenameA, "c") do ds
+        defDim(ds, "lon", sz[1])
+        defDim(ds, "lat", sz[2])
+        v = defVar(ds, "var", Float64, ("lon", "lat"))
+        v[:, :] = data
+        v = defVar(ds, "var2", Float64, ("lon", "lat"))
+        v[:, :] = 2 * data
+    end
+    NCDataset(filenameB, "c") do ds
+        defDim(ds, "lon", sz[1])
+        defDim(ds, "lat", sz[2])
+        v = defVar(ds, "var", Float64, ("lon", "lat"))
+        v[:, :] = data
+        v = defVar(ds, "var2", Float64, ("lon", "lat"))
+        v[:, :] = 2 * data
+    end
+
+    best_mse = OrderedCollections.OrderedDict("var" => 0, "var2" => 0)
+
+    computed_mse = NCRegressionTests.compute_mse(;
+        job_name = "NCTest",
+        best_mse,
+        ds_filename_computed = filenameA,
+        ds_filename_reference = filenameB,
+    )
+    @test computed_mse["var"] == 0
+    @test computed_mse["var2"] == 0
+
+end
+
+@testset "NCRegressionTests - Mismatch" begin
+
+    sz = (123, 145)
+    data = randn(MersenneTwister(152), sz)
+
+    filenameA = tempname()
+    filenameB = tempname()
+
+    NCDataset(filenameA, "c") do ds
+        defDim(ds, "lon", sz[1])
+        defDim(ds, "lat", sz[2])
+        v = defVar(ds, "var", Float64, ("lon", "lat"))
+        v[:, :] = data .+ 1
+        v = defVar(ds, "var2", Float64, ("lon", "lat"))
+        v[:, :] = 2 * data .+ 2
+    end
+    NCDataset(filenameB, "c") do ds
+        defDim(ds, "lon", sz[1])
+        defDim(ds, "lat", sz[2])
+        v = defVar(ds, "var", Float64, ("lon", "lat"))
+        v[:, :] = data
+        v = defVar(ds, "var2", Float64, ("lon", "lat"))
+        v[:, :] = 2 * data
+    end
+
+    best_mse = OrderedCollections.OrderedDict("var" => 2.8633275236008583e+04, "var2" => 0)
+
+    computed_mse = NCRegressionTests.compute_mse(;
+        job_name = "NCTest",
+        best_mse,
+        ds_filename_computed = filenameA,
+        ds_filename_reference = filenameB,
+    )
+
+    @test computed_mse["var"] == 2.8633275236008583e+04
+    @test computed_mse["var2"] == 2.8633275236008583e+04
+
+    best_mse = OrderedCollections.OrderedDict("var" => 2.8633275236008583e+04, "var2" => 0, "no_key" => 1)
+
+    err = ErrorException("No key no_key for mse computation.")
+    @test_throws err NCRegressionTests.compute_mse(;
+        job_name = "NCTest",
+        best_mse,
+        ds_filename_computed = filenameA,
+        ds_filename_reference = filenameB,
+    )
+
+end
